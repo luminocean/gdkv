2d39df41aa71a35c8fa424329a30b821c74650e6
luminocean <282896922@qq.com>
2015-11-29 15:54:07 +0800

    结构调整

--- index.js
+++ index.js
@@ -7,30 +7,41 @@ require.config({
 });
 
 var core;
-
 requirejs(["jquery","core"], function($, core) {
     window.core = core;
 
     $(function(){
+        // 显示第 0,1 步
+        $('#headStep').show();
+        $('#tokenStep').show();
+
+        // 如果本地存储里有token，直接填在input里面
         if(localStorage.gdriveToken){
             $("#tokenInput").val(localStorage.gdriveToken);
         }
 
+        // 填写token后，点击done进入下一步
         $("#tokenDoneBtn").on('click', function(){
             var token = $("#tokenInput").val();
-            tokenDone(token);
+            listFiles(token);
         });
     });
 });
 
-// 获取token后进行的动作
-function tokenDone(token){
+function listFiles(token){
+    // 列出所有文件
     core.listFiles(token, function(files){
-        console.log(files);
-
+        $("#fileListStep").show();
         for(var i in files){
             var file = files[i];
             $('#fileListDiv').append('<p><a href="#">' + file.title + '</a></p>');
         }
+
+        // 进入第 2 步，显示键值对输入页面
+        showKV();
     });
+}
+
+function showKV(){
+    $("#kvStep").show();
 }
